f1242a269516751958ee49ad66b8446c53a746e7
mordy <moh.abdelmordy.it@gmail.com>
2021-07-14 11:09:44 +0200

    two

---
+++ lib/gnr/gnrFuntion.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+////  Varibales .......................>
+const Color primaryColor = Color(0xffff0000);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color yellowColor = Color(0xFFD3AF37);
+const yellowColor2 = Color(0xFFFFC12F);
+const BageColor = Color(0xFFFFE6Ac);
+//// End Varibales ......................>
+
+////  Fonts ......................>
+const MyFont="MR_AY";
+////  End Fonts ......................>
